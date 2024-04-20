6f5934efd1ae1cc8604a21cef3a44e5ffbaee902
Arnav Mahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2024-04-20 14:07:54 +0530

    Add files via upload

---
+++ spdfs.py
@@ -0,0 +1,19 @@
+def dfs(graph, start, visited=None):
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    print(start)
+    for neighbor in graph[start]:
+        if neighbor not in visited:
+            dfs(graph, neighbor, visited)
+    return visited
+
+graph = {
+    '0': ['1', '2'],
+    '1': ['0', '3', '4'],
+    '2': ['0'],
+    '3': ['1'],
+    '4': ['2', '3']
+}
+
+dfs(graph, '0')
